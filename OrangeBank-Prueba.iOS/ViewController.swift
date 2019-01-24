5a6d67f4601afd7ce967c32fbb4492d12ab2efd2
Enrique <easureda@gmail.com>
2019-01-24 11:45:00 +0100

    Initial Commit

---
+++ OrangeBank-Prueba.iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OrangeBank-Prueba.iOS
+//
+//  Created by Enrique Alfonso on 24/01/2019.
+//  Copyright © 2019 Enrique Sureda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
